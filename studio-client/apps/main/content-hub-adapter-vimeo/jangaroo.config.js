b19e4f4596366e539f870b4f4e62e04cc1dd65e0
Philipp Hofmann <philipp.hofmann@coremedia.com>
2021-12-14 14:20:45 +0100

    switching to release version

--- studio-client/apps/main/content-hub-adapter-vimeo/jangaroo.config.js
+++ studio-client/apps/main/content-hub-adapter-vimeo/jangaroo.config.js
@@ -1,9 +1,9 @@
 /** @type { import('@jangaroo/core').IJangarooConfig } */
 module.exports = {
   type: "code",
-  extName: "com.coremedia.labs.plugins__studio-client.content-hub-adapter-vimeo",
-  extNamespace: "com.coremedia.labs.plugins.adapters.vimeo",
   sencha: {
+    name: "com.coremedia.labs.plugins__studio-client.content-hub-adapter-vimeo",
+    namespace: "com.coremedia.labs.plugins.adapters.vimeo",
     studioPlugins: [
       {
         mainClass: "com.coremedia.labs.plugins.adapters.vimeo.ContentHubVimeoStudioPlugin",
